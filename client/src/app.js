888b184f7f0c9718b686a20687df2dc8ac7a2750
richa <richa.gupta@iquanti.com>
2016-06-12 07:36:09 +0530

    added generate url option

--- client/src/app.js
+++ client/src/app.js
@@ -3,10 +3,10 @@ var app = angular.module('hackerearth', ['ngRoute']);
 var codetable = {'user_name':'Richa Gupta','user_email':'ric.gupta1103@gmail.com','base_url':'http://127.0.0.1:8000'}
 app.controller('CodeTableController',['$scope','$http', function ($scope,$http) {
     $scope.availableOptions= [
-      {id: 'C', name: 'C (gcc 4.8.4)', async: 0},
-      {id: 'CPP', name: 'C++ (g++ 4.8.4)', async: 1},
-      {id: 'JAVA', name: 'Java (openjdk 1.7.0_91)', async: 1},
-      {id: 'PYTHON', name: 'Python (python 2.7.6)', async: 0}
+      {id: 'C', name: 'C (gcc 4.8.4)'},
+      {id: 'CPP', name: 'C++ (g++ 4.8.4)'},
+      {id: 'JAVA', name: 'Java (openjdk 1.7.0_91)'},
+      {id: 'PYTHON', name: 'Python (python 2.7.6)'}
     ]
     $scope.selectLanguage = 'C'
     $scope.testInput = false
@@ -31,15 +31,9 @@ app.controller('CodeTableController',['$scope','$http', function ($scope,$http) 
         $scope.compiling = true
         $scope.inputData = {}
         $scope.inputData['async'] = 0
-        for(var i=0 ; i<$scope.availableOptions.length; i++){
-            if($scope.availableOptions[i].id == $scope.selectLanguage){
-                $scope.inputData['async'] = $scope.availableOptions[i].async
-            }
-        }
         $scope.inputData['source'] = $scope.source
         $scope.inputData['lang'] = $scope.selectLanguage
         $scope.inputData['input'] = $scope.testInputText
-        $scope.inputData['client_secret'] = 'ef01eee83b0251ca94f8012763b15273ba4705b3'
         $scope.inputData['time_limit'] = 5
         $scope.inputData['memory_limit'] = 262144
         // calling the api
@@ -50,16 +44,6 @@ app.controller('CodeTableController',['$scope','$http', function ($scope,$http) 
         }).then(
             function (result) {
                 $scope.data = result.data
-//                if($scope.inputData['async']){
-//                    $http({
-//                        method: 'GET',
-//                        url: codetable.base_url+'/hackerearth-response',
-//
-//                    }).then(
-//                        function (result) {
-//
-//                        })
-//                }
                 $scope.compiling = false
                 $scope.viewResult = true
                 if($scope.data.run_status.status == 'CE'){
